712ad049fad58e81e7ea62a1c711a59a1d9c69de
tothantal <43417284+tothantal@users.noreply.github.com>
2020-09-16 17:24:04 +0200

    Create liskov.java

---
+++ liskov.java
@@ -0,0 +1,34 @@
+public class  {
+
+    public static class Vehicle() {
+        System.out.println("Vehicle");
+    
+        public void start() {
+            System.out.println("Starting Vehicle");
+        }        
+    }
+    
+    public static class Car extends Vehicle {
+        System.out.println("Car");
+    
+        @Override
+        public void start() {
+            System.out.println("Starting Car");
+        }        
+    }
+    
+    public static class Supercar extends Car {
+        System.out.println("Supercar");
+    
+        @Override
+        public void start() {
+            System.out.println("Starting Supercar");
+        }   
+    }    
+}
+
+public static class Main() {
+    public static void main(String[] args) {
+        
+    }
+}
