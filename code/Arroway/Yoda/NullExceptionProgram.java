4abdc8d53db5c3a6e3dd78ee5aaea70eedbe5ce3
tothantal <43417284+tothantal@users.noreply.github.com>
2020-12-09 12:45:14 +0100

    Delete NullExceptionProgram.java

--- code/Arroway/Yoda/NullExceptionProgram.java
+++
@@ -1,8 +0,0 @@
-public class NullExceptionProgram {
-	public static void main(String []args) {
-		String mytring = null;
-		if (!("foobar".equals(myString))) {
-			System.out.println("It works!");  // Does not work
-		}
-	}
-}
